575b42ea489cad7b6208a29d99c777c5769c0f54
Guang <chenguang.cs@gmail.com>
2019-10-15 00:48:47 -0400

    Add files via upload

---
+++ chessercise.py
@@ -0,0 +1,69 @@
+import sys
+
+def list_move(x, y, chess_type):
+	base_dir = [(1,0), (0,1), (-1,0), (0,-1)]
+	dia_dir = [(1,1), (1,-1), (-1,1), (-1,-1)]
+	kni_dir = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
+	if(chess_type.lower() == 'queen'):
+		yield from move(x, y, base_dir + dia_dir, 8)
+	elif(chess_type.lower() == 'knight'):
+		yield from move(x, y, kni_dir, 1)
+	elif(chess_type.lower() == 'rook'):
+		yield from move(x, y, base_dir, 8)
+	elif(chess_type.lower() == 'bishop'):
+		yield from move(x, y, dia_dir, 8)
+	elif(chess_type.lower() == 'king'):
+		yield from move(x, y, base_dir + dia_dir, 1)
+	else:
+		print("No such chess type defined")
+
+def move(x, y, directions, step):
+	if(step == 1):
+		for dir_x, dir_y in directions:
+			if(isValid(x+dir_x, y+dir_y)):
+				yield to_chess(x+dir_x, y+dir_y)
+	else:
+		for dir_x, dir_y in directions:
+			yield from move_along(x, y, dir_x, dir_y)
+
+def move_along(x, y, dir_x, dir_y):
+	while(True):
+		x += dir_x
+		y += dir_y
+		if(isValid(x, y)):
+			yield to_chess(x, y)
+		else:
+			break
+
+def isValid(x, y):
+	if(x < 0 or x > 7 or y < 0 or y > 7):
+		return False
+	return True
+
+def to_chess(x, y):
+	return chr(ord('a')+x) + str(y+1)
+
+def from_chess(pos):
+	return (ord(pos[0])-ord('a'), int(pos[1])-1)
+
+def usage():
+	print("chessercise.py -piece KNIGHT -position d2")
+
+def main():
+	if((len(sys.argv) != 5) or (sys.argv[1] != "-piece") or (sys.argv[3] != "-position")):
+		usage()
+		return
+
+	chess_type = sys.argv[2]
+	position = sys.argv[4]
+
+	x, y = from_chess(position)
+	if(not isValid(x,y)):
+		print("Not valid staring position")
+		return
+	for pos in list_move(x, y, chess_type):
+		print(pos, end = ',')
+	sys.stdout.write("\b \n")
+
+if __name__ == "__main__":
+	main()
---
+++ guess.py
@@ -0,0 +1,78 @@
+class GuessNumber:
+	def __init__(self, max):
+		self.max = max
+		self.guess_num = -1
+		self.guess_times = 0
+		self.game_times = 0
+		self.avg = 0
+
+	def main_loop(self):
+		while(True):
+			while(self.game_times != 0):
+				decide = input("Play again?")
+				if(decide in ['y', 'n']):
+					break
+				else:
+					print("Please input 'y'--[yes] or 'n'--[no]")
+			if((self.game_times == 0) or decide == 'y'):
+				self.start_guess()
+			else:
+				return
+
+	def start_guess(self):
+		low = 1
+		high = self.max
+		while(low <= high):
+			if(low == high):
+				self.guess_num = low
+				self.finish_print()
+				return
+			n = self.guess(low, high)
+			while(True):
+				decide = input("%d ?" % n)
+				if(decide not in ['h', 'l', 'c']):
+					print("Please input 'l'--[low] or 'h'--[high] or 'c'--[equal]")
+				elif(((n == low) and (decide == 'h')) or ((n == high) and (decide == 'l'))):
+					print("As guessed number already touch limit %d, you cannot go beyond" % n)
+				else:
+					break
+			if(decide == 'h'):
+				high = n - 1
+				self.guess_times += 1
+			elif(decide == 'l'):
+				low = n + 1
+				self.guess_times += 1
+			else:
+				self.guess_times += 1
+				self.guess_num = n
+				self.finish_print()
+				return
+			assert(low <= high)
+
+	def guess(self, low, high):
+		return (low + high) // 2
+
+	def finish_print(self):
+		print("Your number is %d." % self.guess_num)
+		print("It took me %d guesses." % self.guess_times)
+		total = self.game_times * self.avg + self.guess_times
+		self.game_times += 1
+		self.avg = total/float(self.game_times)
+		print("I averaged %f guesses per game for %d game(s)." % (self.avg, self.game_times))
+		self.guess_times = 0
+
+def main():
+	while(True):
+		try:
+			n = int(input("Please enter a number n: "))
+			if(n <= 0):
+				print("Input should be a valid positive number")
+			else:
+				break
+		except ValueError:
+			print("That was no valid number.  Try again...")
+	game = GuessNumber(n)
+	game.main_loop()
+
+if __name__ == "__main__":
+	main()
